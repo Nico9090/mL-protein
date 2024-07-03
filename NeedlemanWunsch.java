2b6b62a17554e933cfcc964db3bde07e3a56088f
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-07-03 18:35:33 -0400

    Create  NeedlemanWunsch.java

---
+++ NeedlemanWunsch.java
@@ -0,0 +1,21 @@
+//  NeedlemanWunsch.java
+import java.util.Scanner;
+public class NeedlemanWunsch{
+        // Establishing scoring rules
+        private static final int match = 2;
+        private static final int mismatch = -1;
+        private static final int gap= -2;
+
+        //Creating a function to define the matrix and sequence alignment
+        public static void main(String args[]){
+                Scanner in = new Scanner(System.in);
+                System.out.print("What is the first sequence? ");
+                String seq1= in.nextLine();
+                System.out.print("What is the second sequence? ");
+                String seq2= in.nextLine();
+
+                // matrix set-up
+
+        }
+
+}
