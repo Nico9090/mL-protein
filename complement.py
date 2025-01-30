bcbef4616cf36136af19a27f9cfc7b5f3de2064b
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-30 09:51:38 -0500

    Update complement.py

--- complement.py
+++ complement.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-with open('sample_DNA.txt','r')as f:
-        sequence=''
-        for line in f:
-                sequence+=line
-        sequence=sequence.rstrip('\n')
-f.close()
-base_dict={'A':'T','T':'A','G':'C','C':'G'}
-compl=''
-for base in sequence:
-        compl+=base_dict[base]
-f=open('DNA_compl.txt','w')
-print(sequence,file=f)
-print(compl,file=f)
-f.close()
+nucl_dict={"A":"T","C":"G","T":"A","G":"C"}
+seq="AgACTCATCTCATTCCACgAAgCTATTTACATACACCTTgTCgCTgACTCCC"
+seq=seq.upper()
+complement=[]
+for nucleotide in seq:
+        complement.append(nucl_dict[nucleotide])
+complement=''.join(complement)
+print(complement)
